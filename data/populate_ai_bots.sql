de431298630b54197b4234060e61d8ace47a1a8f
Alex Forascu <alexforascu@gmail.com>
2018-03-12 16:04:36 +0200

    Add SQL script to populate ai_bots table

---
+++ data/populate_ai_bots.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+--ADD GERMAN BATTLE COMMANDERS
+INSERT INTO ai_bots (name, team, iq) VALUES ('Adolf Hitler', '0', '200');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Friedrich Paulus', '0', '180');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Erich von Manstein', '0', '160');
+INSERT INTO ai_bots (name, team, iq) VALUES ('W.F. von Richthofen', '0', '140');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Petre Dumitrescu', '0', '130');
+INSERT INTO ai_bots (name, team, iq) VALUES ('C. Constantinescu', '0', '120');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Italo Gariboldi', '0', '100');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Gusztav Jany', '0', '80');
+
+--ADD SOVIET BATTLE COMMANDERS
+INSERT INTO ai_bots (name, team, iq) VALUES ('Georgy Zhukov', '1', '200');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Nikolay Voronov', '1', '180');
+INSERT INTO ai_bots (name, team, iq) VALUES ('A.M. Vasilevsky', '1', '160');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Andrey Yeryomenko', '1', '140');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Nikita Khrushchev', '1', '130');
+INSERT INTO ai_bots (name, team, iq) VALUES ('K.K. Rokossovsky', '1', '120');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Nikolai Vatutin', '1', '100');
+INSERT INTO ai_bots (name, team, iq) VALUES ('Vasily Chuikov', '1', '80');
+
+END;
